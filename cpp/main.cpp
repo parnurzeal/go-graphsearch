2dd02ccf4a73da7334ddab510823f5ff5d447f6e
parnurzeal <parnurzeal@gmail.com>
2014-08-29 18:07:08 +0900

    Finally found E in cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,25 +5,62 @@
 
 using namespace std;
 
-class Node{
+/*class Node{
   public:
     Node *up, *down, *left, *right;
     int state;
     enum State { WALL, SPACE, START, END };
-};
+};*/
 
+typedef struct {
+  bool passed;
+  char state;
+  int preR, preC;
+  enum State { WALL, SPACE, START, END };
+} Node;
+
+void DFS(vector<vector<Node> > &all_nodes, int preR, int preC, int incR, int incC){
+  int r = preR + incR, c = preC +incC;
+  if(r<0 || r>=all_nodes.size() || c <0 || c>=all_nodes[0].size() | all_nodes[r][c].passed ){
+    return;
+  }
+  all_nodes[r][c].passed = true;
+  all_nodes[r][c].preR = preR; all_nodes[r][c].preC = preC;
+  if( all_nodes[r][c].state == 'E' ){
+    cout<< "foundE";
+    return;
+  }
+  DFS(all_nodes,r,c,-1,0);
+  DFS(all_nodes,r,c,0,-1);
+  DFS(all_nodes,r,c,1,0);
+  DFS(all_nodes,r,c,0,1);
+}
 
 int main(){
-  vector<vector<char> > inputs;
+  vector<vector<Node> > all_nodes;
   string line;
   while(getline(cin, line)){
     istringstream iss(line);
+    vector<Node> line_ins;
     for(int i= 0 ;i<line.length();i++){
-      cout<<line[i];
+      Node tmp; tmp.state = line[i];
+      line_ins.push_back(tmp);
     }
-    cout<<endl;
+    all_nodes.push_back(line_ins);
   }
-   
-  cout<<"Hello world!";
+  int rSize = all_nodes.size();
+  int cSize = all_nodes[0].size();
+  int rStart =0, cStart =0;
+  for(int i = 0 ;i<rSize;i++){
+    for(int j = 0;j<cSize;j++){
+      all_nodes[i][j].passed=false;
+      if(all_nodes[i][j].state=='S'){
+        rStart = i; cStart =j;
+      }
+    }
+  }
+
+  DFS(all_nodes,rStart, cStart,0,0);
+
   return 0;
 }
